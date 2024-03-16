9537951758449d0b80bb2f7dd53479a63b069252
OverlordCrowley <maksim2004@bk.ru>
2024-03-16 20:18:42 +0500

    Added websocket

--- index.js
+++ index.js
@@ -1,89 +1,82 @@
-require('dotenv').config()
-const express = require('express')
-const sequelize = require('./db')
-const cors = require('cors')
-const fileUpload = require('express-fileupload')
-const router = require('./routes/index')
-const path = require('path')
+const express = require('express');
+const http = require('http');
+const socketIO = require('socket.io');
+const sequelize = require('./db');
+const cors = require('cors');
+const fileUpload = require('express-fileupload');
+const router = require('./routes/index');
+const path = require('path');
 const cluster = require("cluster");
 const process = require("process");
 const os = require("os");
-const http = require('http');
-const WebSocket = require('ws');
-const {logger} = require("sequelize/lib/utils/logger");
+require('dotenv').config()
 
-const PORT = process.env.PORT || 8080
+const PORT = process.env.PORT || 8080;
 const cpus = os.cpus;
 const numCPUs = cpus().length;
-const websocketApp = express()
-const websocketServer = http.createServer(websocketApp)
-const wss = new WebSocket.Server({server: websocketServer})
+
+const app = express();
+const server = http.createServer(app);
+const io = socketIO(server, {
+  cors: {
+    origin: "http://localhost:4200",
+    methods: ["GET", "POST"]
+  }
+});
+let socketConnected = new Set()
+
+app.use(cors({
+  origin: 'http://localhost:4200',
+}));
+app.use(express.json());
+app.use('/static', express.static(path.join(__dirname, 'static')));
+app.use(fileUpload());
+app.use('/api', router);
+
+
+
+
+io.on('connection', (socket) => {
+  console.log(socket.id);
+  socketConnected.add(socket.id)
+
+
+
+  socket.on('message', (message) => {
+    console.log('Message received:', message);
+    io.emit('message', message);
+  });
+
+  socket.on('disconnect', () => {
+    console.log('Socket disconnected ', socket.id);
+    socketConnected.delete(socket.id)
+  });
+});
+
+
 
 
 if (cluster.isPrimary) {
   console.log(`Primary ${process.pid} is running`);
-
 
   for (let i = 0; i < numCPUs; i++) {
     cluster.fork();
   }
 
   cluster.on("exit", (worker, code, signal) => {
-    // cluster.fork();
     console.log(`worker ${worker.process.pid} died`);
   });
-}
-else{
-  const app = express()
-  app.use(cors())
-  app.use(express.json())
-
-
-
-  app.use('/static', express.static(path.join(__dirname, 'static')));
-
-  app.get('/:filename', (req, res) => {
-    const filename = req.params.filename;
-    const photoUrl = `/static/${filename}`;
-    res.send({ photoUrl });
-  });
-
-  app.use(fileUpload({}))
-  app.use('/api', router)
-
-
+} else {
   const start = async () => {
     try {
-      await sequelize.authenticate()
-      await sequelize.sync()
-
-      app.listen(PORT, () => console.log(`Server started on port ${PORT}`))
+       sequelize.authenticate();
+       sequelize.sync();
+      server.listen(PORT, () => {
+        console.log(`Server started on port ${PORT}`);
+      });
     } catch (e) {
-      console.log(e)
+      console.log(e);
     }
-
-  try{
-      wss.on("connection", function connection(ws){
-
-        console.log("A new client connected")
-        ws.send("welcome new client")
-
-        ws.on("message", function incoming(message){
-          console.log("received: %s ", message)
-          ws.send("Got u r mess its: ", message)
-        })
-
-      })
-
-    websocketServer.listen(3000, () => console.log("Listening on port 3000"))
-  }
-  catch (e) {
-
-  }
-    
-
-  }
-
-
-  start()
+  };
+  start();
 }
